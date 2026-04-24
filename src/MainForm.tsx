a27e58a419c7ecc8de1169176804719205d31a0e
Christian Chaux <xwilarg@protonmail.com>
2026-04-24 07:17:26 +0000

    Fix video update behavior

--- src/MainForm.tsx
+++ src/MainForm.tsx
@@ -15,7 +15,46 @@ let characters = [
     "sigma",
     "winston",
     "wrecking_ball",
-    "zarya"
+    "zarya",
+
+    "anran",
+    "ashe",
+    "bastion",
+    "cassidy",
+    "echo",
+    "emre",
+    "freja",
+    "genji",
+    "hanzo",
+    "junkrat",
+    "mei",
+    "pharah",
+    "reaper",
+    "sierra",
+    "sojourn",
+    "soldier_76",
+    "sombra",
+    "symmetra",
+    "torbjorn",
+    "tracer",
+    "vendetta",
+    "venture",
+    "widowmaker",
+
+    "ana",
+    "bastion",
+    "brigitte",
+    "illari",
+    "jetpack_cat",
+    "juno",
+    "kiriko",
+    "lifeweaver",
+    "lucio",
+    "mercy",
+    "mizuki",
+    "moira",
+    "wuyang",
+    "zenyatta"
 ];
 
 interface VideoData
@@ -31,14 +70,17 @@ export default function MainForm() {
         {
             video ?
             <div className="container box ">
-                <video controls>
+                <video controls key={`${video.user}/${video.character}`}>
                     <source src={`/data/${video.user}/${video.character}.mp4`} type="video/mp4" />
                 </video>
             </div>
             : <></>
         }
         <div className="container box flex">
-            { characters.map(x => <CharacterForm setVideo={(user) => { setVideo({ character: x, user: user }) }} name={x} displayName={x} />) }
+            { characters.map(x => <CharacterForm setVideo={(user) => {
+                setVideo({ character: x, user: user })
+                window.scrollTo({ top: 0, behavior: 'smooth' })
+            }} name={x} displayName={x} />) }
         </div>
     </>
 }
